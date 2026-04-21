215420b9b147b468648a60c1e6494ec34e22c8b1
AKSAN01 <santivargas222@gmail.com>
2026-04-21 02:25:37 -0500

    expansion de galeria

--- src/data.js
+++ src/data.js
@@ -12,4 +12,52 @@ export const memories = [
     message: "Uno de los momentos mas increibles de nuestra relacion, algo que recuerdo con amor, nostalgia y calidez, viajar, dormir a tu lado, conocer a tu familia, mostrarme algo tan intimo, son sensaciones que quedaron tatuadas a fuego en mi alma y me mostro lo especial que me haces sentir"
   },
   
+    {
+    id: 3,
+    imageUrl: "/Papas.jpeg",
+    date: "Tus padres",
+    message: "Solo pocas veces habia sentido tantos nervios como el dia que conoci a tus padres, sentir la presion y la buscar la aprobacion de tus padres fue algo que me puso de los pelos, pero lograrlo no solo me dio tranquilidad, me dio la seguridad de que podria seguir a tu lado por mucho tiempo"
+  },
+
+    {
+    id: 4,
+    imageUrl: "/Primerdibujo.jpeg",
+    date: "Una Flor amarilla",
+    message: "Una de las flores mas especiales que di en mi vida, solo que esta jamas se marchitara, pensar y tomarme el tiempo de dibujarla para darte me hizo reflexionar de una cosa importante, mi corazon deseaba tu ser"
+  },
+
+    {
+    id: 5,
+    imageUrl: "/MunditoMC.jpeg",
+    date: "Nuestro Primer Mundo",
+    message: "Dejar volar nuestra creatividad, crear, juntar y plasmar nuestras ideas en uno de los juegos favoritos es algo tan especial que nunca pude vivir de forma tan personal con alguien mas, cumpliendo uno de los sueños que mi yo de niño no creia poder cumplir "
+  },
+
+    {
+    id: 6,
+    imageUrl: "/Ajedrez.jpeg",
+    date: "Nuestra primera velada",
+    message: "La noche que lo empezo todo, algo de imprevisto, una casualidad que formo 365 dias de historias, risas, lagrimas y momentos de una forma tan inesperada, haber disfrutado de una partida de ajedrez con alguien que marcaria mi vida, es algo que no me pude llegar ni a imaginar"
+  },
+
+    {
+    id: 7,
+    imageUrl: "/Cine.jpeg",
+    date: "El septimo arte",
+    message: "Una escapada que recuerdo con gran detalle, vivir contigo y con mariana 2 peliculas con el riesgo de esconder comida, corriendo entre centros comerciales y terminando el dia junto a ti es algo que me gustaria repetir, para sentir esa parte tan comica y espontanea de tu ser"
+  },
+
+    {
+    id: 8,
+    imageUrl: "/beimax.jpeg",
+    date: "Manos llenas de magia",
+    message: "Dia a dia me sorprendo de la habilidad que tienes en tus manos para crear tan increbles obras, una muestra no solo de tu destreza, si no de tu compromiso y tu aprendizaje autonomo, mostrandome tu capacidad sin limites"
+  },
+
+    {
+    id: 9,
+    imageUrl: "/Neeko.jpg",
+    date: "La primera foto",
+    message: "La foto que dio pie a una historia tan variada y tan linda que le dio un rumbo de 180 grados a lo que hasta ese momento era mi vida, una foto que sin pensarlo daria lugar a dar todo mi amor a un ser humano tan especial sin darme cuenta que la amaria con toda mi vida"
+  },
 ];
