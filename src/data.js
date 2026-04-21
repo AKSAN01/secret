12a1422a197dfc3077af5bd061e9794819e2b09f
AKSAN01 <santivargas222@gmail.com>
2026-04-21 02:39:00 -0500

    data mod

--- src/data.js
+++ src/data.js
@@ -14,42 +14,42 @@ export const memories = [
   
     {
     id: 3,
-    imageUrl: "/Papas.jpeg",
+    imageUrl: "/Papas.jpg",
     date: "Tus padres",
     message: "Solo pocas veces habia sentido tantos nervios como el dia que conoci a tus padres, sentir la presion y la buscar la aprobacion de tus padres fue algo que me puso de los pelos, pero lograrlo no solo me dio tranquilidad, me dio la seguridad de que podria seguir a tu lado por mucho tiempo"
   },
 
     {
     id: 4,
-    imageUrl: "/Primerdibujo.jpeg",
+    imageUrl: "/Primerdibujo.jpg",
     date: "Una Flor amarilla",
     message: "Una de las flores mas especiales que di en mi vida, solo que esta jamas se marchitara, pensar y tomarme el tiempo de dibujarla para darte me hizo reflexionar de una cosa importante, mi corazon deseaba tu ser"
   },
 
     {
     id: 5,
-    imageUrl: "/MunditoMC.jpeg",
+    imageUrl: "/MunditoMC.jpg",
     date: "Nuestro Primer Mundo",
     message: "Dejar volar nuestra creatividad, crear, juntar y plasmar nuestras ideas en uno de los juegos favoritos es algo tan especial que nunca pude vivir de forma tan personal con alguien mas, cumpliendo uno de los sueños que mi yo de niño no creia poder cumplir "
   },
 
     {
     id: 6,
-    imageUrl: "/Ajedrez.jpeg",
+    imageUrl: "/Ajedrez.jpg",
     date: "Nuestra primera velada",
     message: "La noche que lo empezo todo, algo de imprevisto, una casualidad que formo 365 dias de historias, risas, lagrimas y momentos de una forma tan inesperada, haber disfrutado de una partida de ajedrez con alguien que marcaria mi vida, es algo que no me pude llegar ni a imaginar"
   },
 
     {
     id: 7,
-    imageUrl: "/Cine.jpeg",
+    imageUrl: "/Cine.jpg",
     date: "El septimo arte",
     message: "Una escapada que recuerdo con gran detalle, vivir contigo y con mariana 2 peliculas con el riesgo de esconder comida, corriendo entre centros comerciales y terminando el dia junto a ti es algo que me gustaria repetir, para sentir esa parte tan comica y espontanea de tu ser"
   },
 
     {
     id: 8,
-    imageUrl: "/beimax.jpeg",
+    imageUrl: "/beimax.jpg",
     date: "Manos llenas de magia",
     message: "Dia a dia me sorprendo de la habilidad que tienes en tus manos para crear tan increbles obras, una muestra no solo de tu destreza, si no de tu compromiso y tu aprendizaje autonomo, mostrandome tu capacidad sin limites"
   },
